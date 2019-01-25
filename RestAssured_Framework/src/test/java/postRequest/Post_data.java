896e02213aa7c5162562ab279103f04b3aefd803
namangupta1010 <namangupta1010@gmail.com>
2019-01-25 12:06:58 +0530

    Api

---
+++ RestAssured_Framework/src/test/java/postRequest/Post_data.java
@@ -0,0 +1,15 @@
+package postRequest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Post_data {
+
+	
+	public void testResponse()
+	{
+		Response resp =RestAssured.get("");
+		int code =resp.getStatusCode();
+		System.out.println(code);
+	}
+}
