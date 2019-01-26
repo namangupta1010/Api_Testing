e2fecd9e190bd5e450b8c65c7c23ab989fc03177
namangupta1010 <namangupta1010@gmail.com>
2019-01-26 23:39:10 +0530

    post

--- RestAssured_Framework/src/test/java/postRequest/Post_data.java
+++ RestAssured_Framework/src/test/java/postRequest/Post_data.java
@@ -1,16 +1,29 @@
 package postRequest;
 
+import java.io.File;
+
+import java.io.FileNotFoundException;
+import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class Post_data {
 
-	
-	public void testResponse()
+	@Test
+	public void testResponse() throws FileNotFoundException
 	{
-		Response resp =RestAssured.get("");
-		int code =resp.getStatusCode();
-		System.out.println(code);
-         //RestAssured.ge
+		RequestSpecification request =RestAssured.given();
+		request.header("Content-Type","application/json");
+		request.header("api-key", "ElJkhJuQIRoFq/kDEblco4LpZqRCdYNIoAVG7SywSXw");
+		File file =new File("C:\\Users\\HP\\Desktop\\jsonshipengine.json");
+		request.body(file);
+		
+		Response response =request.post("https://api.shipengine.com/v1/rates");
+		System.out.println(response.getStatusCode());
+		System.out.println(response.asString());
+		/*JSONObject json = new JSONObject();
+		json.*/
 	}
 }
